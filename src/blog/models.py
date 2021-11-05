20bc5466ae4f45bd8a7e3c468f7383ab17d4d3a3
Sofia <sofiadurkan@gmail.com>
2021-11-05 16:41:50 -0500

    Category field created

--- src/blog/models.py
+++ src/blog/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+
+class Meta:
+    verbose_name_plural = 'Categories'
+
 class Post(models.Model):
     OPTIONS = (
         ('draft', 'Draft'),
@@ -19,4 +23,3 @@ class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     status = models.CharField(max_length=10,choices=OPTIONS,default='draft')
     slug = models.SlugField(blank=True,unique=True)
-
